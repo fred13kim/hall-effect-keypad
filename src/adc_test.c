890a847978eb8cb1d115c19ab733560569410d40
fred13kim <fred13kim@gmail.com>
2025-11-17 23:23:40 -0500

    Add working adc-test

--- src/adc_test.c
+++ src/adc_test.c
@@ -9,9 +9,18 @@
 
 int main(void){
     stdio_init_all();
+
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+
+    printf("=== ADC TEST ===");
+
     while(true) {
-        printf("Senior Projects!\n");
-        sleep_ms(1000);
+        const float conversion_factor = 3.3f / (1 << 12);
+        uint16_t res = adc_read();
+        printf("RAW: 0x%03x, VOLTS: %f V\n", res, res * conversion_factor);
+        sleep_ms(1);
     }
 }
 
