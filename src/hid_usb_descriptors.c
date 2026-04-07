c95df5d9b7e63aed7e330532d7fe51ce45d7d59d
atkwon <atkwon20@gmail.com>
2026-04-07 01:05:02 -0400

    write usb descriptors for hid use

---
+++ src/hid_usb_descriptors.c
@@ -0,0 +1,130 @@
+#include "tusb.h"
+
+// HID Report Descriptor
+// Treat as joystick with analog axis 
+uint8_t const desc_hid_report[] = {
+    HID_USAGE_PAGE(HID_USAGE_PAGE_DESKTOP),
+    HID_USAGE(HID_USAGE_DESKTOP_GAMEPAD),
+    HID_COLLECTION(HID_COLLECTION_APPLICATION),
+        HID_USAGE_PAGE(HID_USAGE_PAGE_DESKTOP),
+        HID_USAGE(HID_USAGE_DESKTOP_X),
+        HID_LOGICAL_MIN(0),
+        HID_LOGICAL_MAX_N(4095, 2),
+        HID_REPORT_SIZE(16),
+        HID_REPORT_COUNT(1),
+        HID_INPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE),
+    HID_COLLECTION_END
+};
+
+#define EPNUM_HID         0x81
+#define CONFIG_TOTAL_LEN  (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
+
+#define PID_MAP(itf, n)   ((CFG_TUD_##itf) ? (1u << (n)) : 0u)
+#define USB_PID           (0x4000 | PID_MAP(CDC, 0) | PID_MAP(MSC, 1) | \
+                           PID_MAP(HID, 2) | PID_MAP(MIDI, 3) | PID_MAP(VENDOR, 4))
+#define USB_VID           0xCafe
+
+enum {
+    ITF_NUM_HID = 0,
+    ITF_NUM_TOTAL
+};
+
+// Device Descriptor
+tusb_desc_device_t const desc_device = {
+    .bLength            = sizeof(tusb_desc_device_t),
+    .bDescriptorType    = TUSB_DESC_DEVICE,
+    .bcdUSB             = 0x0200,
+    .bDeviceClass       = 0x00,
+    .bDeviceSubClass    = 0x00,
+    .bDeviceProtocol    = 0x00,
+    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
+    .idVendor           = USB_VID,
+    .idProduct          = USB_PID,
+    .bcdDevice          = 0x0100,
+    .iManufacturer      = 0x01,
+    .iProduct           = 0x02,
+    .iSerialNumber      = 0x03,
+    .bNumConfigurations = 0x01
+};
+
+uint8_t const* tud_descriptor_device_cb(void) {
+    return (uint8_t const*) &desc_device;
+}
+
+uint8_t const desc_configuration[] = {
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0,
+		    	  CONFIG_TOTAL_LEN,
+			  0x00, 100),
+    
+    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE,
+                       sizeof(desc_hid_report), EPNUM_HID,
+                       CFG_TUD_HID_EP_BUFSIZE, 10)
+};
+
+uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
+    (void) index;
+    return desc_configuration;
+}
+
+uint8_t const* tud_hid_descriptor_report_cb(uint8_t instance) {
+    (void) instance;
+    return desc_hid_report;
+}
+
+// String Descriptors
+static char const* string_desc_arr[] = {
+    (const char[]) { 0x09, 0x04 },
+    "Analog Hall-Effect Keypad",
+    "Pico2 HID",
+    "0001",
+    "HID Interface"
+};
+
+static uint16_t _desc_str[32];
+
+uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+    (void) langid;
+    
+    uint8_t chr_count;
+    
+    if (index == 0) {
+        _desc_str[1] = 0x0409;
+        chr_count = 1;
+    } else {
+        if (index >= sizeof(string_desc_arr)/sizeof(string_desc_arr[0]))
+            return NULL;
+        
+	const char* str = string_desc_arr[index];
+        
+	chr_count = 0;
+        while (str[chr_count] && chr_count < 31) {
+            _desc_str[1 + chr_count] = (uint16_t) str[chr_count];
+            chr_count++;
+        }
+    }
+
+    _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * chr_count + 2);
+    return _desc_str;
+}
+
+// Required but not used
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id,
+                            hid_report_type_t report_type,
+                            uint8_t const* buffer, uint16_t bufsize) {
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) bufsize;
+}
+
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id,
+                                hid_report_type_t report_type,
+                                uint8_t* buffer, uint16_t reqlen) {
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+    return 0;
+}
