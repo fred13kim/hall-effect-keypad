abddda64f8b2e640bc09e50df10748aff5454ac8
fred13kim <fred13kim@gmail.com>
2026-04-10 18:26:51 -0400

    feat: add channel defs

--- lib/mcp3008_spi/include/mcp3008_spi.h
+++ lib/mcp3008_spi/include/mcp3008_spi.h
@@ -3,6 +3,19 @@
 #include "hardware/spi.h"
 #include "hardware/gpio.h"
 
+
+// typedefs
+typedef enum {
+    MCP3008_CH0 = 0,
+    MCP3008_CH1,
+    MCP3008_CH2,
+    MCP3008_CH3,
+    MCP3008_CH4,
+    MCP3008_CH5,
+    MCP3008_CH6,
+    MCP3008_CH7,
+} mcp3008_channel_t;
+
 typedef struct {
     spi_inst_t *spi_dev;
     uint baudrate;
@@ -12,6 +25,8 @@ typedef struct {
     uint sck_pin;
 } mcp3008_inst_t;
 
+
 void mcp3008_init(mcp3008_inst_t *mcp_dev);
+void mcp3008_read(mcp3008_inst_t *mcp_dev);
 
 #endif // !MCP_3008_H
