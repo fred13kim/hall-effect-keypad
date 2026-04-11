319fd7b744ef3507e68fcb6142569dfffd3daa25
fred13kim <fred13kim@gmail.com>
2026-04-10 21:19:33 -0400

    feat: working single shot adc

--- lib/mcp3008_spi/mcp3008_spi.c
+++ lib/mcp3008_spi/mcp3008_spi.c
@@ -1,12 +1,13 @@
 #include "mcp3008_spi.h"
+#include <stdio.h>
 
 #define RATE_100_KHZ 100000
 
 void mcp3008_init(mcp3008_inst_t *mcp_dev) {
     spi_init(mcp_dev->spi_dev, mcp_dev->baudrate);
 
-    gpio_set_function(mcp_dev->miso_pin, GPIO_FUNC_SPI);
     gpio_set_function(mcp_dev->mosi_pin, GPIO_FUNC_SPI);
+    gpio_set_function(mcp_dev->miso_pin, GPIO_FUNC_SPI);
     gpio_set_function(mcp_dev->sck_pin, GPIO_FUNC_SPI);
 
     gpio_init(mcp_dev->cs_pin);
@@ -28,6 +29,12 @@ void mcp3008_read(mcp3008_inst_t *mcp_dev, mcp3008_channel_t ch) {
         0x00,       // XX don't care
     };
 
+    uint8_t rx[3] = {0};
+
     gpio_put(mcp_dev->cs_pin, 0);
-    spi_write_blocking(mcp_dev->spi_dev, tx, sizeof(tx));
-    gpio_put(mcp_dev->cs_pin, 1); }
+    spi_write_read_blocking(mcp_dev->spi_dev, tx, rx, sizeof(tx));
+    gpio_put(mcp_dev->cs_pin, 1);
+
+    uint16_t ret = ((rx[1] & 0x03) << 8) | rx[2];
+    printf("Dout: %d\n", ret);
+}
