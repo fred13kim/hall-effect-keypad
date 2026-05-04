3aeacd4b7a850f965d1416706ce8b11b32cdd2e6
fred13kim <fred13kim@gmail.com>
2026-05-04 14:52:35 -0400

    feat: add init headers

--- lib/ssd1306_i2c/include/ssd1306_i2c.h
+++ lib/ssd1306_i2c/include/ssd1306_i2c.h
@@ -1,5 +1,15 @@
 #ifndef SSD1306_I2C_H
 #define SSD1306_I2C_H
+#include "hardware/i2c.h"
+
+typedef struct {
+    i2c_inst_t *i2c_dev;
+    uint baudrate;
+    uint sda_pin;
+    uint scl_pin;
+} ssd1306_inst_t;
+
+void ssd1306_init(ssd1306_inst_t *ssd_dev);
 
 #endif // !SSD1306_I2C_H
 
