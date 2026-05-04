00ec71f6a0bc1aa359b677b3388942303918ae11
Andrew <andrew.yuan@cooper.edu>
2026-05-03 22:01:50 -0400

    remove hysterisis + add correct adc thresholds

--- gui/profile_manager.py
+++ gui/profile_manager.py
@@ -7,11 +7,8 @@ class ProfileManager:
         self.active_profile_id = str(active_profile_id)
         self.profile_data = self.load_profile()
 
-        # Temporary calibration values for now.
-        # Later, these should also come from the profile or calibration UI.
-        self.rest_adc = 550
-        self.max_adc = 720
-
+        self.min_adc = 270
+        self.rest_adc = 500
     def load_profile(self):
         with open(self.profile_path, "r", encoding="utf-8") as file:
             return json.load(file)
@@ -38,32 +35,15 @@ class ProfileManager:
         return self.get_active_profile()["buttons"][button_id]
 
     def adc_to_level(self, adc_value):
-        """
-        Converts raw ADC value to a 0.0–1.0 press level.
 
-        For now:
-            550 -> 0.0
-            720 -> 1.0
-
-        Later, rest_adc and max_adc should come from calibration/profile settings.
-        """
-        if self.max_adc == self.rest_adc:
+        if self.min_adc == self.rest_adc:
             return 0.0
 
-        level = (adc_value - self.rest_adc) / (self.max_adc - self.rest_adc)
+        level = (adc_value - self.min_adc) / (self.rest_adc - self.min_adc)
         return max(0.0, min(1.0, level))
 
     def get_output_from_level(self, button_id, level):
-        """
-        Example:
-            breakpoints = [0.75, 0.5]
-            outputs     = ["A", "B", "C"]
 
-        Mapping:
-            level >= 0.75              -> "A"
-            0.5 <= level < 0.75        -> "B"
-            level < 0.5                -> "C"
-        """
         button_config = self.get_button_config(button_id)
 
         breakpoints = button_config["breakpoints"]
